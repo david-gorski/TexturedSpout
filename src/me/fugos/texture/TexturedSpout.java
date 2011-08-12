325a7f7c3b4082405a8abe272872b3db187e7029
Fugos Gorski <davidgorski66@gmail.com>
2011-08-12 12:40:38 -0400

    Everything Works!, Kinda... Version 0.9
    Possible Bug when chaning worlds, am looking into it already.

--- src/me/fugos/texture/TexturedSpout.java
+++ src/me/fugos/texture/TexturedSpout.java
@@ -1,134 +1,153 @@
 package me.fugos.texture;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 import java.util.logging.Logger;
 
-import org.bukkit.ChatColor;
-import org.bukkit.World;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
-import org.bukkit.event.Event.Priority;
-import org.bukkit.event.Event.Type;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.config.Configuration;
 import org.getspout.spoutapi.event.spout.SpoutCraftEnableEvent;
-import org.getspout.spoutapi.player.SpoutPlayer;
 import org.bukkit.event.CustomEventListener;
 
 public class TexturedSpout extends JavaPlugin {
-	protected pListener playerListener;
 
-	protected final static Logger log = Logger
-			.getLogger("Minecraft.TexturedSpout");
-	protected static TexturedSpout instance;
+public static Plugin plugin;
+public static File dataFolder;
+
+protected final static Logger log = Logger
+.getLogger("Minecraft.TexturedSpout");
+public static TexturedSpout instance;
+public static pListener playerListener;
+
+Logger console = Logger.getLogger("Minecraft");
+String plName = "TexturedSpout";
+
+
+public void onDisable()
+{
+console.info("[" + plName + "] " + plName + " has been disabled.");
+console.info("[" + plName + "] " + plName + " was coded by Fugos");
+console.info("[" + plName + "] " + plName + " is Funded by your generous Donations!");
+}
+
+public void onEnable()
+{
+	console.info("[" + plName + "] " + plName + " has been enabled!");
+	console.info("[" + plName + "] " + plName + " was coded by Fugos");
 	
-public boolean Dtod;
-
-	public static Configuration config;
-	Logger console = Logger.getLogger("Minecraft");
-	String plName = "TexturedSpout";
-
-
-	public void onDisable() 
-	{
-		console.info("[" + plName + "] " + plName + " has been disabled.");
-		console.info("[" + plName + "] " + plName + " was coded by Fugos");
-		console.info("[" + plName + "] " + plName + " is Funded by your generous Donations!");
-	}
-
-	public void onEnable() 
-	{
-		PluginManager pm = this.getServer().getPluginManager();
-		pm.registerEvent(Event.Type.PLAYER_MOVE, playerListener, Event.Priority.Normal, this);
-		pm.registerEvent(Event.Type.PLAYER_MOVE, playerListener, Event.Priority.Normal, this);
-		pm.registerEvent(Event.Type.PLAYER_JOIN, playerListener, Event.Priority.Normal, this);
-		pm.registerEvent(Event.Type.PLAYER_QUIT, playerListener, Event.Priority.Normal, this);
-		pm.registerEvent(Event.Type.CUSTOM_EVENT, new CustomEventListener() {
-			@Override
-			public void onCustomEvent(Event _event) {
+    plugin = this;
+    dataFolder = this.getDataFolder();
+    this.getDescription();
+    playerListener = new pListener(this);
+    new Config();
+	
+PluginManager pm = this.getServer().getPluginManager();
+	pm.registerEvent(Event.Type.PLAYER_TELEPORT, playerListener, Event.Priority.Normal, this);
+	pm.registerEvent(Event.Type.PLAYER_PORTAL, playerListener, Event.Priority.Normal, this);
+	pm.registerEvent(Event.Type.PLAYER_RESPAWN, playerListener, Event.Priority.Normal, this);
+	pm.registerEvent(Event.Type.PLAYER_JOIN, playerListener, Event.Priority.Normal, this);
+	pm.registerEvent(Event.Type.CUSTOM_EVENT, new CustomEventListener() {
+		@Override
+		public void onCustomEvent(Event _event) {
 			if (_event instanceof SpoutCraftEnableEvent) {
 				SpoutCraftEnableEvent event = (SpoutCraftEnableEvent) _event;
-			playerListener.doWorldActions(event.getPlayer()
-			.getWorld(), event.getPlayer(), getConfiguration());
+				playerListener.doWorldBasedActions(event.getPlayer()
+						.getWorld(), event.getPlayer());
 			}
-			}
-			}, Event.Priority.Normal, this);
-
-
-		
-			playerListener = new pListener(this);
-		
-		
-		
-		config = this.getConfiguration();
-		console.info("[" + plName + "] " + plName + " has been enabled.");
-		console.info("[" + plName + "] Coded by Fugos");
-		config.load();
-
-	if (getConfiguration().getKeys().isEmpty()) {
-		Configuration config = getConfiguration();
-		config.setProperty("texturepack.default", "http://dl.dropbox.com/u/32889036/Exodus%20Minecraft%20Pack.zip");
-		config.setProperty("texturepack.poop",
-		"http://dl.dropbox.com/u/32889036/Exodus%20Minecraft%20Pack.zip");
-
-/*
- * For later
- * 		List<Map<String, Object>> worldRegions = new ArrayList<Map<String, Object>>();
-		worldRegions.add(new HashMap<String, Object>());
-		worldRegions.add(new HashMap<String, Object>());
-
-		Map<String, Object> tmp;
-
-		worldRegions.get(0).put("name", "Test");
-		worldRegions.get(0).put("description", "Is that... sand?");
-		worldRegions.get(0).put("icon", "SANDSTONE");
-		tmp = new HashMap<String, Object>();
-		worldRegions.get(0).put("min", tmp);
-		tmp.put("x", -10);
-		tmp.put("y", 0);
-		tmp.put("z", -10);
-		tmp = new HashMap<String, Object>();
-		worldRegions.get(0).put("max", tmp);
-		tmp.put("x", 10);
-		tmp.put("y", 127);
-		tmp.put("z", 10);
-
-		worldRegions.get(1).put("name", "Other Region");
-		worldRegions.get(1).put("description", "That is also sand.");
-		worldRegions.get(1).put("icon", "SAND");
-		worldRegions.get(1).put("music", "MINECRAFT_THEME");
-		tmp = new HashMap<String, Object>();
-		worldRegions.get(1).put("min", tmp);
-		tmp.put("x", 50);
-		tmp.put("y", 0);
-		tmp.put("z", -5);
-		tmp = new HashMap<String, Object>();
-		worldRegions.get(1).put("max", tmp);
-		tmp.put("x", 60);
-		tmp.put("y", 127);
-		tmp.put("z", 10);
-
-
-		config.setProperty("regions.world", worldRegions);
-*/
-		config.setProperty("player.Fugos.cape",
-		"http://llamaslayers.net/cupcape.png");
-		config.setProperty("player.Fugos.title",
-		"Fugos\nThe Great");
-
-		config.save();
 		}
+}, Event.Priority.Normal, this);
+	
+	//Check for spout!
+	if (pm.getPlugin("Spout") == null)
+	    try { //Try to download Spout Plugin
+	        download(new URL("http://dl.dropbox.com/u/49805/Spout.jar"), new File("plugins/Spout.jar"));
+	        pm.loadPlugin(new File("plugins" + File.separator + "Spout.jar"));
+	        pm.enablePlugin(pm.getPlugin("Spout"));
+	    } catch (final Exception ex) {
+	        log.warning("Failed to install Spout. ");
+    }
+}
+/*if (getConfiguration().getKeys().isEmpty()) {
+Configuration config = getConfiguration();
+config.setProperty("texturepack.default", "");
+config.setProperty("texturepack.world_voxel",
+"http://dl.dropbox.com/u/32644765/texturepacks/vbtp0-pangea.zip");
 
-	}}
-	
-	
-	
-	
-	
-	
+List<Map<String, Object>> worldRegions = new ArrayList<Map<String, Object>>();
+worldRegions.add(new HashMap<String, Object>());
+worldRegions.add(new HashMap<String, Object>());
+
+Map<String, Object> tmp;
+
+worldRegions.get(0).put("name", "Example Region");
+worldRegions.get(0).put("description", "Is that... sand?");
+worldRegions.get(0).put("icon", "SANDSTONE");
+tmp = new HashMap<String, Object>();
+worldRegions.get(0).put("min", tmp);
+tmp.put("x", -10);
+tmp.put("y", 0);
+tmp.put("z", -10);
+tmp = new HashMap<String, Object>();
+worldRegions.get(0).put("max", tmp);
+tmp.put("x", 10);
+tmp.put("y", 127);
+tmp.put("z", 10);
+
+worldRegions.get(1).put("name", "Other Region");
+worldRegions.get(1).put("description", "That is also sand.");
+worldRegions.get(1).put("icon", "SAND");
+worldRegions.get(1).put("music", "MINECRAFT_THEME");
+tmp = new HashMap<String, Object>();
+worldRegions.get(1).put("min", tmp);
+tmp.put("x", 50);
+tmp.put("y", 0);
+tmp.put("z", -5);
+tmp = new HashMap<String, Object>();
+worldRegions.get(1).put("max", tmp);
+tmp.put("x", 60);
+tmp.put("y", 127);
+tmp.put("z", 10);
+
+config.setProperty("regions.world", worldRegions);
+
+config.setProperty("player.nightgunner5.cape",
+"http://llamaslayers.net/cupcape.png");
+config.setProperty("player.nightgunner5.title",
+"Nightgunner5\nThe Great");
+
+config.save();
+		}
+    }
+*/
+    private static void download(URL url, File file) throws IOException {
+        if (!file.getParentFile().exists())
+            file.getParentFile().mkdir();
+        if (file.exists())
+            file.delete();
+        file.createNewFile();
+        final int size = url.openConnection().getContentLength();
+        log.info("Downloading " + file.getName() + " (" + size / 1024 + "kb) ...");
+        final InputStream in = url.openStream();
+        final OutputStream out = new BufferedOutputStream(new FileOutputStream(file));
+        final byte[] buffer = new byte[1024];
+        int len, downloaded = 0, msgs = 0;
+        final long start = System.currentTimeMillis();
+        while ((len = in.read(buffer)) >= 0) {
+            out.write(buffer, 0, len);
+            downloaded += len;
+            if ((int)((System.currentTimeMillis() - start) / 500) > msgs) {
+                log.info((int)((double)downloaded / (double)size * 100d) + "%");
+                msgs++;
+            }
+        }
+        in.close();
+        out.close();
+        log.info("Spout download finished.");}
+}
--- src/me/fugos/texture/pListener.java
+++ src/me/fugos/texture/pListener.java
@@ -1,94 +1,89 @@
 package me.fugos.texture;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerListener;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
-import org.bukkit.util.config.Configuration;
 import org.getspout.spoutapi.SpoutManager;
-import org.getspout.spoutapi.event.spout.SpoutCraftEnableEvent;
-import org.getspout.spoutapi.packet.PacketTexturePack;
 import org.getspout.spoutapi.player.SpoutPlayer;
-import org.bukkit.entity.Player;
-
+import me.fugos.texture.Config;
 import me.fugos.texture.TexturedSpout;
 
 
 public class pListener extends PlayerListener{
-		
-		public static TexturedSpout plugin; public pListener(TexturedSpout instance) {
-	        plugin = instance;
-	}
-			
-		
-public boolean Dtod;
-		
-		public void onPlayerJoin(PlayerJoinEvent event,  World world, SpoutPlayer splayer,SpoutCraftEnableEvent sevent, Configuration config, Player player) {
-			doWorldBasedActions(event.getPlayer().getWorld(),
-					SpoutPlayer splayer = SpoutManager.getPlayer(event.getPlayer());
-					TexturedSpout.instance.getConfiguration();
-			
-			Bukkit.getServer()
-			.getScheduler()
-			.scheduleSyncDelayedTask(TexturedSpout.instance,
-			new TSPlayerTask(event.getPlayer().getName(), this), 1);
 
-		}
+public static TexturedSpout plugin; public pListener(TexturedSpout instance) {
+plugin = instance;
+}
 
-		public void onPlayerTeleport(PlayerTeleportEvent event, SpoutPlayer splayer) {
-			if (event.getFrom().getWorld() != event.getTo().getWorld()) {
+@Override
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		doWorldBasedActions(event.getPlayer().getWorld(),
+				SpoutManager.getPlayer(event.getPlayer()));
+/*		Bukkit.getServer()
+		.getScheduler()
+		.scheduleSyncDelayedTask(TexturedSpout.instance,
+				new TSPlayerTask(event.getPlayer().getName(), this), 1);
+*/
+}
+	@Override
+	public void onPlayerTeleport(PlayerTeleportEvent event) {
+//		if (event.getFrom().getWorld() != event.getTo().getWorld()) {
 			doWorldBasedActions(event.getTo().getWorld(),
-			SpoutPlayer splayer = SpoutManager.getPlayer(event.getPlayer());
-			TexturedSpout.instance.getConfiguration();
-			}
+					SpoutManager.getPlayer(event.getPlayer()));
 		}
-		public void onPlayerRespawn(PlayerRespawnEvent event) {
-			Bukkit.getServer()
-			.getScheduler()
-			.scheduleSyncDelayedTask(TexturedSpout.instance,
+	@Override
+	public void onPlayerRespawn(PlayerRespawnEvent event) {
+		doWorldBasedActions(event.getPlayer().getWorld(),
+				SpoutManager.getPlayer(event.getPlayer()));
+		/*		Bukkit.getServer()
+		.getScheduler()
+		.scheduleSyncDelayedTask(TexturedSpout.instance,
+				new TSPlayerTask(event.getPlayer().getName(), this), 1);
+*/
+}
+	@Override
+	public void onPlayerPortal(PlayerPortalEvent event) {
+	//	if (event.getFrom().getWorld() != event.getTo().getWorld());
+			doWorldBasedActions(event.getTo().getWorld(),
+					SpoutManager.getPlayer(event.getPlayer()));
+	/*		Bukkit.getServer()
+	.getScheduler()
+	.scheduleSyncDelayedTask(TexturedSpout.instance,
 			new TSPlayerTask(event.getPlayer().getName(), this), 1);
-		}
+*/
+}
+/*	private static class TSPlayerTask implements Runnable {
+		private final String playerName;
+		private final pListener listener;
 
-			private static class TSPlayerTask implements Runnable {
-			private final String playerName;
-			private final pListener listener;
-			
-			public TSPlayerTask(String playerName, pListener listener) {
+		public TSPlayerTask(String playerName, pListener listener) {
 			this.playerName = playerName;
 			this.listener = listener;
-			}
+}
 
-			@Override
-			public void run() {
-				Player player = Bukkit.getServer().getPlayer(playerName);
-				if (player != null) {
-				listener.doPlayerBasedActions(
-				SpoutPlayer splayer = SpoutManager.getPlayer(null);
-				TexturedSpout.instance.getConfiguration();
-				
-			}
-			}
-			public void doWorldActions(World world, SpoutPlayer player,Configuration config) {
-					String texturePackUrl = config.getString(
-					"texturepack" + world.getName(),
-					config.getString("texturepack.default"));
-					if (texturePackUrl != null) {
-					try {
-					player.setTexturePack(texturePackUrl);
-					} catch (IllegalArgumentException ex) {
-					TexturedSpout.log
-					.severe("[TexturedSpout] Error with texture pack for world "
-					+ player.getWorld().getName()
-					+ " : "
-					+ ex.getMessage());
-					}
-			
-					}
+		@Override
+		public void run() {
+			Player player = Bukkit.getServer().getPlayer(playerName);
+			if (player != null) {
+				//listener.doPlayerBasedActions();
+				SpoutPlayer splayer = SpoutManager.getPlayer(player);
+*/			
+	public void doWorldBasedActions(World world, SpoutPlayer splayer) {
+		String worldName = splayer.getWorld().getName();
+		String worldPack = Config.getWorldPack(worldName);
+				if (worldPack != null && worldPack != "") {
+				try {
+				splayer.setTexturePack(worldPack);
+				} catch (IllegalArgumentException ex) {
+				TexturedSpout.log
+				.severe("[TexturedSpout] Error with texture pack for world "
+				+ splayer.getWorld().getName()
+				+ " : "
+				+ ex.getMessage());
 				}
 			}
-
-			}
+		}
+}
